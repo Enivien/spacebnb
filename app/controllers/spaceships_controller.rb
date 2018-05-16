23f6458432aaa88cfe9f395f7b2ce8a0d0d70bd3
Irvin Hoogland <irvinhoogland@gmail.com>
2018-05-16 16:11:10 +0800

    Add user model and spaceship model

---
+++ app/controllers/spaceships_controller.rb
@@ -0,0 +1,10 @@
+class SpaceshipsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/new'
+  get 'spaceships/index'
+  get 'spaceships/show'
+  get 'spaceships/new'
   devise_for :users
   root to: 'spaceships#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
