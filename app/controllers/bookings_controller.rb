7ec3b72d3043a4df2dae7c13e2d0fb410fb63300
Irvin Hoogland <irvinhoogland@gmail.com>
2018-05-16 17:34:09 +0800

    Add bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,21 @@
+class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    booking = Booking.new(booking_params)
+    booking.save
+    if booking.save
+      redirect_to booking_path(booking)
+    else
+      render :new
+    end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price, :status, :user, :spaceship)
+  end
+end
--- db/migrate/20180516090219_create_bookings.rb
+++ db/migrate/20180516090219_create_bookings.rb
@@ -4,7 +4,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
       t.date :start_date
       t.date :end_date
       t.integer :total_price
-      t.string :status
+      t.string :status, default: "pending"
       t.references :user, foreign_key: true
       t.references :spaceship, foreign_key: true
 
