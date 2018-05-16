66d10ff7aaf25c3e3845b70b5cfcc592b3e71805
Irvin Hoogland <irvinhoogland@gmail.com>
2018-05-16 18:05:40 +0800

    Add fake faker seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Spaceship.create(
+    name: Faker::HitchhikersGuideToTheGalaxy.starship,
+    location: "space coordinates",
+    picture: "url",
+    capacity: (50..200).to_a.sample,
+    price: (500..10000).to_a.sample
+    )
+  p spaceship.name
+end
+
+# 10.times do
+#   User.create(
+#     name: Faker::HitchhikersGuideToTheGalaxy.character,
+#     email: Faker::Internet.email,
+#     avatar: Faker::Avatar.image("my-own-slug", "50x50"),
+#     description: Faker::HitchhikersGuideToTheGalaxy.quote
+#     )
+# end
