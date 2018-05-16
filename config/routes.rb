adbe7464319318189132aec382b2768ef8a82116
Irvin Hoogland <irvinhoogland@gmail.com>
2018-05-16 17:29:00 +0800

    Add routes booking and review

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/new'
-  get 'spaceships/index'
-  get 'spaceships/show'
-  get 'spaceships/new'
   devise_for :users
   root to: 'spaceships#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :spaceships do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
 end
