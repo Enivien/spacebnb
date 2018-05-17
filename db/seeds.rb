24bdb0bbd65da2670dcf231074606f2befe6cf68
Ryan Clogg <ryan.clogg@gmail.com>
2018-05-17 17:00:15 +0800

    Updated user page - needs some CSS and structure love but she's v functional.

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,24 +30,22 @@ end
   p booking.total_price
 end
 
-# 10.times do
-#   User.create(
-#     name: Faker::HitchhikersGuideToTheGalaxy.character,
-#     email: Faker::Internet.email,
-#     avatar: Faker::Avatar.image("my-own-slug", "50x50"),
-#     description: Faker::HitchhikersGuideToTheGalaxy.quote
-#     )
-# end
-
-  # create_table "bookings", force: :cascade do |t|
-  #   t.date "start_date"
-  #   t.date "end_date"
-  #   t.integer "total_price"
-  #   t.string "status"
-  #   t.bigint "user_id"
-  #   t.bigint "spaceship_id"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["spaceship_id"], name: "index_bookings_on_spaceship_id"
-  #   t.index ["user_id"], name: "index_bookings_on_user_id"
-  # end
+create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.string "avatar"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
