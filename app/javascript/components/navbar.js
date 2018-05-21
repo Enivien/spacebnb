545d48bc152262747dabaf8cdb00256f332eff66
Irvin Hoogland <irvinhoogland@gmail.com>
2018-05-21 16:43:35 +0800

    Add show page and navbar fixes

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,12 +1,18 @@
 function initUpdateNavbarOnScroll() {
+  const body = document.querySelector('body');
+
+  if(!body.classList.contains("home")) return;
+
   const navbar = document.querySelector('.navbar-wagon');
+  navbar.classList.add('navbar-wagon-transparent');
+
   window.addEventListener('scroll', () => {
     if (window.scrollY >= window.innerHeight) {
-      navbar.classList.add('navbar-wagon-white');
-      navbar.classList.add('navbar-wagon-link-white');
+      navbar.classList.remove('navbar-wagon-transparent');
+      // navbar.classList.add('navbar-wagon-link-white');
     } else {
-      navbar.classList.remove('navbar-wagon-white');
-      navbar.classList.add('navbar-wagon-link-white');
+      navbar.classList.add('navbar-wagon-transparent');
+      // navbar.classList.add('navbar-wagon-link-white');
     }
   });
 }
